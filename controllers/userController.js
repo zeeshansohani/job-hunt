bb547a4b175396b7b5a6a33f42568fb0c65fb965
Sahil Sohani <sahilsohani@Sahils-MacBook-Air.local>
2024-08-13 20:52:43 +0500

    Updated backend to use new Netlify URL

--- controllers/userController.js
+++ controllers/userController.js
@@ -100,7 +100,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 100,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
+        secure: true,
       })
       .json({
         message: `Welcome back, ${user.fullname}`,
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json()); // req sent in JSON format
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://job-hunt-now.netlify.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
